c0755d8eb323974a457d4de3fa37de96f152cc66
rtkmor <rtkmor@gmail.com>
2025-09-04 00:44:48 +0530

    feat(medium): ✨ Solve 152. Maximum Product Subarray

---
+++ arrays/medium/0152-maximum-product-subarray/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        int ans = INT_MIN;
+        int prod1 = 1;
+        int prod2 = 1;
+        for(int i=0 ; i<n ; i++){
+            prod1 *= nums[i];
+            prod2 *= nums[n-i-1];
+            ans = max({ans, prod1, prod2});
+            if(prod1==0) prod1 = 1;
+            if(prod2==0) prod2 = 1;
+        }
+        return ans;
+    }
+};
