247171700de4c95f1b54324359ca614d59a36bf6
rtkmor <rtkmor@gmail.com>
2025-09-10 22:55:38 +0530

    feat(medium): ✨ Solve 560. Subarray Sum Equals K

---
+++ arrays/medium/0560-subarray-sum-equals-k/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        int sum = 0;
+        map<int,int> mp;
+        int count = 0;
+
+        for(int i=0 ; i<n ; i++){
+            sum += nums[i];
+            if(sum==k) count++;
+
+            int rem = sum - k;
+            if(mp.find(rem)!=mp.end()){
+                count += mp[rem];
+            }
+
+            mp[sum]++;
+        }
+
+        return count;
+    }
+};
